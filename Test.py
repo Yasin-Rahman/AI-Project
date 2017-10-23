0c9a1e10fd04281a7ad23b3498acf3a4ef7aa20d
Yasin-Rahman <yasinshreshto@gmail.com>
2017-10-24 00:40:53 +0600

    Add files via upload
    Creating the basline file for presentation.

---
+++ Test.py
@@ -0,0 +1,46 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+#import python modules
+#example modules:
+import os
+import urllib
+import numpy as np
+import tensorflow as tf
+import urllib.request
+
+
+# In[ ]:
+
+#Import from Tensorflow
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+# In[ ]:
+
+#Loading dataset
+Training="x.csv"
+Testing="y.csv"
+
+
+# In[ ]:
+
+#Main Body of Code
+#Import Dataset
+#Build Model
+
+
+# In[ ]:
+
+#Train The model
+#test The model
+
+
+# In[ ]:
+
+#Print accuracy and result.
+
